9b2e961bf3f43c35e02823dc5fbaa96e52dbf006
Samuel <padmadansamuel@gmail.com>
2024-07-16 12:18:33 +0530

    Add project files.

---
+++ Models/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp.Models
+{
+    internal class Player
+    {
+        static int DEFAULT_AGE = 18;
+        private int _id; 
+        private string _name;
+        private int _age ;
+
+        public int Id { get { return _id; } }
+
+        public string Name { get { return _name; } }
+
+        public int Age { get { return _age; } }
+
+
+        public Player(int id , string name)
+        {
+            _id = id;
+            _name = name;
+            _age = DEFAULT_AGE;
+
+        }
+        public Player(int id , string name , int age):this(id ,name) {
+            _age = age; 
+        }
+
+        public static Player WhoIsElder(Player[] players)
+        {
+            int maxAge = 0;
+            Player playerWithMaxAge = null;
+            foreach (Player player in players)
+            {
+                if (player.Age > maxAge)
+                {
+                    playerWithMaxAge = player;
+                    maxAge = player.Age;
+                }
+            }
+            return playerWithMaxAge;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Id : {Id} \nName : {Name} \nAge : {Age}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using PlayerApp.Models;
+
+namespace PlayerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player[] players = 
+            {
+                new Player(1 , "Samuel "),
+                new Player(2 , "Arjun" , 23),
+                new Player(3 , "Sam" , 34),
+                new Player(4 , "Saroj" , 34),
+                new Player(5 , "Ramesh" , 56),
+                
+            };
+
+            
+            Player player = Player.WhoIsElder(players);
+            Console.WriteLine("Player with max age : ");
+            Console.WriteLine(player);
+
+            
+            
+        }
+    }
+}
